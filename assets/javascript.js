5712d58557d7916196aa679d64533296b3ae99b0
wilcook <wil.cook.is@gmail.com>
2016-09-18 13:23:06 -0400

    minor change

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.4.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBWmPnBuDlM4Z2N_n1rgzwGh9oYBc-A1fg",
+    authDomain: "metoperatfcb.firebaseapp.com",
+    databaseURL: "https://metoperatfcb.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "308739311794"
+  };
+  firebase.initializeApp(config);
+</script>
