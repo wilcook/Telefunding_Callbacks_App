166ea701849f628573f15e855ee8b4e53679b483
wilcook <wil.cook.is@gmail.com>
2016-09-21 10:47:06 -0400

    fbase functionality added

--- assets/javascript/fbase.js
+++ assets/javascript/fbase.js
@@ -1,10 +1,15 @@
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyBWmPnBuDlM4Z2N_n1rgzwGh9oYBc-A1fg",
-authDomain: "metoperatfcb.firebaseapp.com",
-databaseURL: "https://metoperatfcb.firebaseio.com",
-storageBucket: "",
-messagingSenderId: "308739311794"
-};
+$('document').ready(function(){
 
-firebase.initializeApp(config);
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBWmPnBuDlM4Z2N_n1rgzwGh9oYBc-A1fg",
+    authDomain: "metoperatfcb.firebaseapp.com",
+    databaseURL: "https://metoperatfcb.firebaseio.com",
+    storageBucket: "metoperatfcb.appspot.com",
+    messagingSenderId: "308739311794"
+  };
+  firebase.initializeApp(config);
+
+//closing brackets for document on ready function 
+})
+
